0a1fb513cd4de088ae28b2e431a5953081976f52
김재헌 <fehs0611@gmail.com>
2022-11-25 21:12:34 +0900

    developing...

--- venvs/myarchive/Scripts/activate.bat
+++ venvs/myarchive/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\development\venvs\myarchive
+set VIRTUAL_ENV=D:\development\flaskProject\venvs\myarchive
 
 if not defined PROMPT set PROMPT=$P$G
 
