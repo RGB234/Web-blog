f5bd9d4dec2e6b47224f714da35bfa8fe9e35ae9
김재헌 <fehs0611@gmail.com>
2022-11-29 21:12:57 +0900

    developing...

--- myarchive/app/models.py
+++ myarchive/app/models.py
@@ -1,7 +1,8 @@
 from app import db #__init__.py 에서 생성한 SQLALchemy 객체
 
 class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True) #primary_key 속성의 기본 키(db상의 id)로 지정
+    userid = db.Column(db.String(60), unique=True, nullable=False)
     username = db.Column(db.String(60), unique=True, nullable=False)
     password = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(100), unique=True, nullable=False)
--- myarchive/app/views/login_views.py
+++ myarchive/app/views/login_views.py
@@ -19,7 +19,7 @@ def _login():
     #request는 flask에서 자동제공 참조:https://velog.io/@sangmin7648/%EC%98%A4%EB%8A%98%EC%9D%98-%EB%B0%B0%EC%9B%80-045
     if request.method == 'POST' and form.validate_on_submit(): #UserLoginForm()을 채운 뒤 로그인버튼을 누르면
         error = None
-        user = User.query.filter_by(username=form.username.data).first() #검색
+        user = User.query.filter_by(userid=form.userid.data).first() #검색
         if not user:
             error = "해당 사용자 계정이 존재하지 않습니다"
         elif not check_password_hash(user.password, form.password.data):
@@ -29,11 +29,11 @@ def _login():
             session.clear()
             '''flask session에 key가 'user_id'이고 value(키값)는 user.id 인 (딕셔너리와 유사? 한)데이터 저장'''
             '''session은 request와 마찬가지로 플라스크에서 자동으로 '''
-            session['user_id'] = user.id
+            session['user_id'] = user.userid
             '''라우트함수에서 역으로 url 주소추출 후 이 url 주소로 모듈연결'''
             '''mypage_views.py의 bp이름이 mypage, mapage_views.py의 라우트 함수들중 하나의 이름이 homepage'''
             '''mypage_views.py의 (라우트)함수 homepage 호출, flask 서버 구동중에는 영구히 사용 가능'''
-            return redirect(url_for('mypage.homepage', user_id=user.id))
+            return redirect(url_for('mypage.homepage', user_id=user.userid))
     #request.method == 'GET'인 경우, (GET 요청 방식인 경우), 로그인 시도 없이 _login함수가 불려왔을 때
     return render_template('login.html', form=form)
 
@@ -43,7 +43,7 @@ def signup():
     form = UserCreateForm()
     if request.metho == 'POST' and form.validate():
         error = None
-        user = User.query.filter_by(username=form.username.data)
+        user = User.query.filter_by(userid=form.userid.data).first()
         if not user:
             user = User(username=form.username.data,
                         password=generate_password_hash(form.password1.data),
