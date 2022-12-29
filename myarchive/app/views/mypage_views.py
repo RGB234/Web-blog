d97f9bbe10e213958c4a9c7679dd02af244c8a0b
RGB234 <fehs0611@gmail.com>
2022-12-29 17:09:10 +0900

    developing...

--- myarchive/app/views/mypage_views.py
+++ myarchive/app/views/mypage_views.py
@@ -13,7 +13,7 @@ def homepage(user_name):
     page = request.args.get('page', type=int, default=1) #URL 에 page 값이 없으면 자동으로 1 적용
     #GET요청방식 URL에서 'page'값을 가져옴 ex)localhost:5000/mypage/<username>/?page=5
     
-    posting_list = Post.query.order_by(Post.create_date.desc())
+    posting_list = Post.query.filter_by(user_id=g.user.id).order_by(Post.create_date.desc())
 
     posting_list = posting_list.paginate(page = page, per_page=10) #posting_list 는 단순 Post 모델이 아니라 Pagination 객체가 된다
     username = User.query.filter_by(username=user_name).first().username
